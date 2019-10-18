428cf8dfe76b62a7dae049b4e08458738483a72c
Simon Lara <simon.larah@gmail.com>
2019-10-17 21:58:33 -0300

    final dia 2

--- src/models.py
+++ src/models.py
@@ -2,16 +2,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    national_id = db.Column(db.Integer, unique=True, nullable=True)
+    lastName = db.Column(db.String(80), nullable=True)
+    names = db.Column(db.String(80),  nullable=False)
+    sex = db.Column(db.String(3),  nullable=False)
+    birthday = db.Column(db.Date,  nullable=False)
+    #address = db.Column(db.String(100),  nullable=False)
+    village_id = db.Column(db.Integer,nullable=False)
+    
+    def __repr__(self):
+        return '<Persons %r>' % self.id
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
-
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+             "id": self.id,
+             "national_id": self.national_id,
+             "lastName": self.emlastNameail,
+             "names": self.names,
+             "sex": self.sex,
+             "birthday": self.birthday,
+             "village_id": self.village_id
+        }
