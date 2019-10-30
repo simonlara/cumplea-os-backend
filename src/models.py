873cf561f30707bb6b622f9bf07e0c9dc525c994
Andrés S <sotomayor001@gmail.com>
2019-10-30 17:05:40 +0000

    Cambios Andres

--- src/models.py
+++ src/models.py
@@ -6,20 +6,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1621@localhost/cumple'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-from sqlalchemy import CHAR, Column, Date, ForeignKey, String, TIMESTAMP, Text, text
-from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy import CHAR, Column, DECIMAL, Date, ForeignKey, String, TIMESTAMP, Text, text
+from sqlalchemy.dialects.mysql import INTEGER, TINYINT
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 metadata = Base.metadata
 
-class Client(Base):
-    __tablename__ = 'clients'
+class Country(Base):
+    __tablename__ = 'countries'
 
     id = Column(INTEGER(11), primary_key=True)
-    rol_id = Column(String(45), nullable=False)
+    country = Column(String(45))
+
 
 class Role(Base):
     __tablename__ = 'roles'
@@ -28,12 +28,6 @@ class Role(Base):
     name = Column(String(120))
     code = Column(String(6))
 
-class Status(Base):
-    __tablename__ = 'status'
-
-    id = Column(INTEGER(11), primary_key=True)
-    Clients_User = Column(INTEGER(11), nullable=False)
-    status = Column(String(45), nullable=False)
 
 class Typecontact(Base):
     __tablename__ = 'typecontact'
@@ -41,30 +35,23 @@ class Typecontact(Base):
     id = Column(INTEGER(11), primary_key=True)
     name = Column(String(45))
 
+
 class Typesrelative(Base):
     __tablename__ = 'typesrelatives'
 
     id = Column(INTEGER(11), primary_key=True)
     name = Column(String(45), nullable=False)
 
-class Village(Base):
-    __tablename__ = 'villages'
 
-    id = Column(INTEGER(11), primary_key=True)
+class Region(Base):
+    __tablename__ = 'regions'
 
-class Person(Base):
-    __tablename__ = 'persons'
+    id = Column(INTEGER(11), primary_key=True, nullable=False)
+    region = Column(String(45))
+    countries_id = Column(ForeignKey('countries.id'), primary_key=True, nullable=False, index=True)
 
-    id = Column(INTEGER(11), primary_key=True)
-    national_id = Column(INTEGER(11))
-    lastname = Column(String(45), nullable=False)
-    name = Column(String(45), nullable=False)
-    sex = Column(CHAR(3))
-    birthday = Column(Date, nullable=False)
-    address = Column(String(45))
-    villages_id = Column(ForeignKey('villages.id'), nullable=False, index=True)
+    countries = relationship('Country')
 
-    villages = relationship('Village')
 
 class User(Base):
     __tablename__ = 'users'
@@ -78,22 +65,61 @@ class User(Base):
 
     roles = relationship('Role')
 
+class Client(Base):
+    __tablename__ = 'clients'
+
+    id = Column(INTEGER(11), primary_key=True)
+    nombre = Column(String(45))
+    rut = Column(String(45))
+    direccion = Column(String(45))
+    website = Column(String(45))
+    email = Column(String(45))
+    phone = Column(String(45))
+    users_id = Column(ForeignKey('users.id'), nullable=False, index=True)
+
+    users = relationship('User')
+
+
+class Village(Base):
+    __tablename__ = 'villages'
+
+    id = Column(INTEGER(11), primary_key=True, nullable=False)
+    village = Column(String(45))
+    regions_id = Column(ForeignKey('regions.id'), primary_key=True, nullable=False, index=True)
+
+    regions = relationship('Region')
+
+
 class Campaign(Base):
     __tablename__ = 'campaigns'
 
-    id = Column(INTEGER(11), primary_key=True, nullable=False)
+    id = Column(INTEGER(11), primary_key=True)
+    endDate = Column(Date)
+    budget = Column(DECIMAL(10, 0), nullable=False)
+    villages_id = Column(ForeignKey('villages.id'), nullable=False, index=True)
     days_before = Column(INTEGER(11))
-    Log_in_User = Column('Log-in_User', String(45), nullable=False)
     sms = Column(String(30))
     mail = Column(Text)
-    villages_id = Column(ForeignKey('villages.id'), nullable=False, index=True)
-    Clients_User = Column(ForeignKey('clients.id'), nullable=False, index=True)
-    users_id = Column(ForeignKey('users.id'), nullable=False, index=True)
-    status_id = Column(ForeignKey('status.id'), primary_key=True, nullable=False, index=True)
+    admin_id = Column(ForeignKey('users.id'), nullable=False, index=True)
+    client_id = Column(ForeignKey('users.id'), nullable=False, index=True)
 
-    client = relationship('Client')
-    status = relationship('Status')
-    users = relationship('User')
+    admin = relationship('User', primaryjoin='Campaign.admin_id == User.id')
+    client = relationship('User', primaryjoin='Campaign.client_id == User.id')
+    villages = relationship('Village')
+
+
+class Person(Base):
+    __tablename__ = 'persons'
+
+    id = Column(INTEGER(11), primary_key=True)
+    national_id = Column(INTEGER(11))
+    lastname = Column(String(45), nullable=False)
+    name = Column(String(45), nullable=False)
+    sex = Column(CHAR(3))
+    birthday = Column(Date, nullable=False)
+    address = Column(String(45))
+    villages_id = Column(ForeignKey('villages.id'), index=True)
+
     villages = relationship('Village')
 
 
@@ -120,3 +146,16 @@ class Family(Base):
     Persons = relationship('Person', primaryjoin='Family.Persons_id == Person.id')
     Relative = relationship('Person', primaryjoin='Family.Relative_id == Person.id')
     typesRelatives = relationship('Typesrelative')
+
+
+class Report(Base):
+    __tablename__ = 'reports'
+
+    id = Column(INTEGER(11), primary_key=True)
+    persons_id = Column(ForeignKey('persons.id'), nullable=False, index=True)
+    campaigns_id = Column(ForeignKey('campaigns.id'), nullable=False, index=True)
+    email_sent = Column(TINYINT(4))
+    sms_sent = Column(TINYINT(4))
+
+    campaigns = relationship('Campaign')
+    persons = relationship('Person')
